6a4956f52325ead2697eb25cfdcd3ed65c505157
BigRedS <ialoneambest@gmail.com>
2014-04-29 12:05:40 +0100

    Added support for remote hosts; fixed /user/ paths

--- lib/sitecreator/scripts/create_unix_user.sh
+++ lib/sitecreator/scripts/create_unix_user.sh
@@ -2,6 +2,14 @@
 
 USERNAME=$1
 PASSWORD=$2
+HOST=$3
 
-/user/sbin/useradd -s /bin/bash -m $USERNAME
-echo $USERNAME:$PASSWORD | /usr/sbin/chpasswd
+
+/usr/sbin/useradd -s /bin/bash -m '$USERNAME'
+echo '$USERNAME:$PASSWORD' | /usr/sbin/chpasswd
+
+
+if [ ! -z $HOST ]; then
+	ssh $HOST "/usr/sbin/useradd -s /bin/bash -m '$USERNAME'"
+	ssh $HOST "echo '$USERNAME:$PASSWORD' | /usr/sbin/chpasswd"
+fi
