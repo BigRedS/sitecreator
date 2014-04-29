d271556d8121714e5102efb19d674e6bc8b3feca
BigRedS <ialoneambest@gmail.com>
2014-04-29 14:16:10 +0100

    Fixed create_mysql_remotely script

--- lib/sitecreator/scripts/create_mysql_remotely.sh
+++ lib/sitecreator/scripts/create_mysql_remotely.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 if [ -z $REMOTE_HOST ]; then
-	print "ERROR: No remote host set (use 'remote_host' env variable"
+	echo "ERROR: No remote host set (use 'remote_host' env variable)"
 	exit 1;
 fi
 
-ssh $REMOTE_HOST "lib/sitecreator/scripts/create_mysql.sh $@"
+ssh $REMOTE_HOST "/usr/lib/sitecreator/scripts/create_mysql.sh $@"
